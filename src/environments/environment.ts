d23c5ebe6b4cdd235c9ac2427a631bf36c44ba6e
l3miage-elboucha <elbouchrifianas@yahoo.com>
2023-03-10 11:06:27 +0100

    add component acceuil and acc config

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'miahoot-9e337',
+    appId: '1:1041958349572:web:abffa5b63681d1b42efe51',
+    storageBucket: 'miahoot-9e337.appspot.com',
+    apiKey: 'AIzaSyB1mXJgJsxo8uf9Cu-xIxSKdEiKlH2wmTI',
+    authDomain: 'miahoot-9e337.firebaseapp.com',
+    messagingSenderId: '1041958349572',
+  },};
