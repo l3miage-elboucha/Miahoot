8e9a4f2f523b059b4b6aef1e08fdbffe25cb15f2
l3miage-elboucha <elbouchrifianas@yahoo.com>
2023-03-31 01:18:55 +0200

    update

--- src/app/account-config/account-config.component.ts
+++ src/app/account-config/account-config.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DataService } from '../data.service';
+import { MiahootUser } from '../miahootUser';
 
 @Component({
   selector: 'app-account-config',
@@ -6,6 +9,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./account-config.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AccountConfigComponent {
 
+  public fg!: FormGroup<{
+    name: FormControl<string>,
+    photoURL: FormControl<string>,
+    photoFile: FormControl<File | undefined>
+  }>;
+
+  public user: MiahootUser | undefined;
+
+  constructor(fb: FormBuilder, data: DataService){
+    this.fg = fb.nonNullable.group({
+      name: [""],
+      photoURL: [""],
+      photoFile: [undefined as File | undefined]
+    })  
+  }
 }
